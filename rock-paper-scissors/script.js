f3c8794371d12326e01cb63cac5751cc464e0527
ffuszthaler <flo.fuszthaler@gmail.com>
2021-11-15 12:23:05 +0100

    added logic

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,34 +1,31 @@
-let rockBtn = document.querySelector(".rockBtn");
-let paperBtn = document.querySelector(".paperBtn");
-let scissorsBtn = document.querySelector(".scissorsBtn");
-let lizardBtn = document.querySelector(".lizardBtn");
-let spockBtn = document.querySelector(".spockBtn");
+let rockBtn = document.querySelector("#rockBtn");
+let paperBtn = document.querySelector("#paperBtn");
+let scissorsBtn = document.querySelector("#scissorsBtn");
+let lizardBtn = document.querySelector("#lizardBtn");
+let spockBtn = document.querySelector("#spockBtn");
 
-let cpuPlayer = () => {
+let result = document.querySelector("#result");
+
+// CPU
+const cpuPlayer = () => {
   let randomPickNumber = Math.floor(Math.random() * 5);
-  // console.log(randomPickNumber);
 
   let randomPick = "";
 
   switch (randomPickNumber) {
     case 0:
-      // console.log("Rock");
       randomPick = "Rock";
       break;
     case 1:
-      // console.log("Paper");
       randomPick = "Paper";
       break;
     case 2:
-      // console.log("Scissors");
       randomPick = "Scissors";
       break;
     case 3:
-      // console.log("Lizard");
       randomPick = "Lizard";
       break;
     case 4:
-      // console.log("Spock");
       randomPick = "Spock";
       break;
 
@@ -39,5 +36,95 @@ let cpuPlayer = () => {
 
   return randomPick;
 };
+let cpuPick = cpuPlayer();
 
-console.log(cpuPlayer());
+// User
+let handleUserInput = (pick) => {
+  let userPick = pick;
+
+  // Winner decision
+  makeDecision(userPick, cpuPick);
+};
+
+rockBtn.addEventListener("click", () => handleUserInput(rockBtn.value));
+paperBtn.addEventListener("click", () => handleUserInput(paperBtn.value));
+scissorsBtn.addEventListener("click", () => handleUserInput(scissorsBtn.value));
+lizardBtn.addEventListener("click", () => handleUserInput(lizardBtn.value));
+spockBtn.addEventListener("click", () => handleUserInput(spockBtn.value));
+
+/*
+ * This black magic works.
+ * It is the mathematical representation of the game mechanics.
+ */
+const evaluateWinner = function (player1, player2) {
+  let difference = player1 - player2;
+  if (difference < 0) {
+    difference += 5;
+  }
+  if (player1 === player2) {
+    tie();
+  } else {
+    if (difference % 2) {
+      playerWin();
+    } else {
+      cpuWin();
+    }
+  }
+};
+
+// Decide who won
+let makeDecision = (user, cpu) => {
+  if (user === cpu) {
+    result.innerText = "Tie";
+  } else if (user === "Rock") {
+    if (cpu === "Paper") {
+      result.innerText = "You Lost";
+    } else if (cpu === "Scissors") {
+      result.innerText = "You Win";
+    } else if (cpu === "Lizard") {
+      result.innerText = "You Win";
+    } else {
+      result.innerText = "You Lost";
+    }
+  } else if (user === "Paper") {
+    if (cpu === "Rock") {
+      result.innerText = "You Win";
+    } else if (cpu === "Scissors") {
+      result.innerText = "You Lost";
+    } else if (cpu === "Lizard") {
+      result.innerText = "You Lost";
+    } else {
+      result.innerText = "You Win";
+    }
+  } else if (user === "Scissors") {
+    if (cpu === "Rock") {
+      result.innerText = "You Lost";
+    } else if (cpu === "Paper") {
+      result.innerText = "You Win";
+    } else if (cpu === "Lizard") {
+      result.innerText = "You Win";
+    } else {
+      result.innerText = "You Lost";
+    }
+  } else if (user === "Lizard") {
+    if (cpu === "Rock") {
+      result.innerText = "You Lost";
+    } else if (cpu === "Paper") {
+      result.innerText = "You Win";
+    } else if (cpu === "Scissors") {
+      result.innerText = "You Lost";
+    } else {
+      result.innerText = "You Win";
+    }
+  } else if (user === "Spock") {
+    if (cpu === "Rock") {
+      result.innerText = "You Win";
+    } else if (cpu === "Paper") {
+      result.innerText = "You Lost";
+    } else if (cpu === "Scissors") {
+      result.innerText = "You Win";
+    } else {
+      result.innerText = "You Lost";
+    }
+  }
+};
